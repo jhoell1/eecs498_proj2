c5a41e7c1c2b7236c9c994fda5e7c668f70919c6
wying523 <wying@umich.edu>
2014-11-20 21:29:19 -0500

    animated plot of the kinematics
    add animation

--- matlab_modelling/rrp.m
+++ matlab_modelling/rrp.m
@@ -4,12 +4,22 @@ close all
 N = 50;
 theta1=linspace(0,2*pi,N);
 theta2=linspace(0,pi/2,N);
-d =linspace(0.05,0.1,N);
+d =linspace(0.01,0.1,N);
+
+%initial frame
+figure(1);hold on
+axis([-0.13 0.13 -0.13 0.13 -0.13, 0.13]);
+view([60,30])
 
 for k=1:N
     
     gst(:,k)=forwardKinematics(theta1(k),theta2(k),d(k));
-
+    l1 = plot3([0 0],[0,0],[0,0.1]);
+    
+    l2 = plot3([0 gst(1,k)],[0,gst(2,k)],[0.1,gst(3,k)],'-o');
+    
+    drawnow
+    pause(0.1)
     
 end
 
@@ -17,5 +27,10 @@ X = gst(1,:);
 Y = gst(2,:);
 Z = gst(3,:);
 figure
-plot3(X,Y,Z)
+comet3(X,Y,Z)
 
+
+
+
+
+ 
