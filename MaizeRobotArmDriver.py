d5f2705af04370c5810b892a5bfe3cc5e9c0d69e
Jakob <jhoell@umich.edu>
2014-12-06 14:56:54 -0500

    updated driver

--- MaizeRobotArmDriver.py
+++ MaizeRobotArmDriver.py
@@ -10,7 +10,7 @@ import ckbot.logical as L
 
 # enum values for app state
 manual_angle = 0
-manual_position = 1
+pos_set = 1
 automatic = 2
 angle_set = 3
 
@@ -41,7 +41,7 @@ class joint( object ):
 
 class robotArmDriver ( JoyApp ):
 
-    def __init__(self,*arg, **kw):
+    def __init__(self,height,A,*arg, **kw):
         JoyApp.__init__(self, *arg,**kw)
         C = L.Cluster(count=3)
         self.servo = [joint(C.at.Nx0E,[-150.0,150.0]),joint(C.at.Nx1C,[-150.0,150.0]),joint(C.at.Nx07,[-150.0,150.0])]
@@ -52,6 +52,8 @@ class robotArmDriver ( JoyApp ):
         self.trajectories = [[],[],[]]
         self.trajectoryIndex = 0
         self.trajectoryMax = 0
+        self.h = height
+        self.A = A
 
     def onStart(self):
         self.controlUpdate = self.onceEvery(1.0/20.0)
@@ -85,13 +87,17 @@ class robotArmDriver ( JoyApp ):
                 # start following trajectory
                 self.followTrajectory = True
                 self.trajectoryIndex = 0
+
+                if self.mode == angle_set:
+                    desiredAngle = self.angleCommand
+                elif self.mode == pos_set:
+                    desiredAngle = self.inverse_kinematics(self.angleCommand,0.1)
+
                 for i in range(0,3):
                     start = [0,self.curConfig[i]]
-                    end = [2000,self.angleCommand[i]]
+                    end = [2000,desiredAngle[i]]
                     self.trajectories[i] = self.linear_interp(start,end,200)
                     self.trajectoryMax = len(self.trajectories[i])
-
-
 
         if self.controlUpdate():
             print "current config" + str(self.curConfig)
@@ -101,7 +107,7 @@ class robotArmDriver ( JoyApp ):
                 #print(self.angleCommand)
                 if self.mode == manual_angle:
                     self.servo[i].move(self.angleCommand[i],False)
-                elif self.mode == angle_set:
+                elif self.mode == angle_set or self.mode == pos_set:
                     if self.followTrajectory == True:
                         self.servo[i].move(self.trajectories[i][self.trajectoryIndex],False)
 
@@ -110,7 +116,101 @@ class robotArmDriver ( JoyApp ):
                 if self.trajectoryIndex == self.trajectoryMax:
                     self.followTrajectory = False
 
+     # ee_pos is a 3 element array ==> [x y z]
+    # step_length should be small, on the order of 0.05 - 0.5
+    def inverse_kinematics(self,desired_ee_pos,step_length):
+
+        step_config = self.curConfig
+        cur_ee_pos = self.end_effector_position(step_config)
+
+        # iterate until we have achieved our goal
+        while sqrt(sum(square(subtract(cur_ee_pos,desired_ee_pos)))) > 0.5 :
+            self.computeNumericJacobian(step_config,step_config)
+            # step in the direction of the gradient that will minimize the
+            # error between the desired and current end effector position
+            step_config = step_config + step_length*dot(self.NumericJacobian,subtract(desired_ee_pos,cur_ee_pos))
+            cur_ee_pos = self.end_effector_position
+
+        self.cur_config = step_config
+        return step_config
+
+    #configuration is an 3 element array ==> [theta1,theta2,theta3]
+    def end_effector_position(self,configuration):
+        [T,J] = self.kinematics(self.curConfig)
+        return dot(T,array([[0],[0],[0],[1]]))[0:3]
+
+    def computeNumericJacobian(self,config):
+        for i in range(1,3): # each of the three joint variables
+            delta = zeros(shape=(1,3))
+            delta[i] = 0.1
+            self.NumericJacobian[i,:] = calcInducedVelocity(delta,config)
+
+        print self.NumericJacobian
+    # delta is an 1 by 3 array
+    # technically, delta should be all zero except for 1 element
+    # that makes velocity calcuation easier
+    # calculates slope using symmetric differnce
+    def calcInducedVelocity(self,delta,config): 
+
+        # check if delta is the right shape
+        if sum(delta!=0) > 1 :
+            print "delta passed to calcInducedVelocities has the incorrect form!"
+            return asanarray([0,0,0])
+
+        c1 = config - delta
+        c2 = config + delta
+
+        p1 = self.end_effector_position(c1)
+        p2 = self.end_effector_position(c2)
+
+        # is correct only if delta has only 1 nonzero #!!
+        return subtract(p2,p1)/(2*sum(delta)) 
+
+    def kinematics(self,config):
+        A = self.A
+        h = self.h
+        t1 = config[0]
+        t2 = config[1]
+        t3 = config[2]
+
+        para = 0.08889*25.4*360/(2*pi);
+        T01 = array([[cos(t1+pi/2),0, sin(t1+pi/2), 0],
+               [sin(t1+pi/2), 0, -cos(t1+pi/2) , 0],
+               [0,      1,        0, h],
+               [0,       0,        0, 1]])
+        T12 = array([[ cos(t2-pi/2),0,sin(t2-pi/2),(4*A)*cos(t2-pi/2)],
+                [sin(t2-pi/2),0,-cos(t2-pi/2),(4*A)*sin(t2-pi/2)],
+                [0,1,0,0],
+                [0,0,0,1]])
+
+        T23 = array([[0, 0, 0,0],
+                [0, 0, 0, 0],
+                [0, 0, 0, para*t3],
+                [0,0,0,1]])
+        T02 = dot(T01,T12)
+        T03 = dot(T02,T23)
+
+        O0 = array([0, 0,0])
+        O1 = T01[0:3,3]
+        O2 = T02[0:3,3]
+        O3 = T03[0:3,3]
+
+        Z0 = array([0,0,1]);
+        Z1 = T01[0:3,2]
+        Z2 = T02[0:3,2]
+        Z3 = T03[0:3,2]
+
+        Jv1 = cross(Z0,O3-O0)
+        Jv2 = cross(Z1,O3-O1)
+        Jv3 = cross(Z2,O3-O2)
+        JV = hstack([Jv1, Jv2, Jv3])
+        JV = (JV)
+
+        return [T03,JV]
+
 
 if __name__ == "__main__":
-    app = robotArmDriver()
+    A = 4*2.54 # inches
+    height = 4*A
+    app = robotArmDriver(height,A)
     app.run()
